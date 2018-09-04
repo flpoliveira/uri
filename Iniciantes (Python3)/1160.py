8b809f36014005e4f34698f5bb0f7df736881804
Filipe <filipedasilvadeoliveira@gmail.com>
2018-09-04 14:26:24 -0300

    1160 Crescimento Populacional

--- Iniciantes (Python3)/1160.py
+++ Iniciantes (Python3)/1160.py
@@ -1,16 +1,16 @@
 for x in range ((int(input()))):
 	entrada = input().split(" ")
-	A = float(entrada[0])
-	B = float(entrada[1])
+	A = int(entrada[0])
+	B = int(entrada[1])
 	pA = float(entrada[2])
 	pB = float(entrada[3])
 
 	i = 0
 
-	while(A <= B):
+	while(A <= B and i < 101):
 		i += 1
-		A += int((pA/100) * A)
-		B += int((pB/100) * B)
+		A += int(pA*A/100)
+		B += int(pB*B/100)
 
 	if(i > 100):
 		print("Mais de 1 seculo.")
